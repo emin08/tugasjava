9169ad01fa26b6f1219f5504837fb0000c98cecf
emin08 <47098649+emin08@users.noreply.github.com>
2019-01-28 13:31:59 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uasbil2;
+import uasbil.nilai;
+
+/**
+ *
+ * @author emin
+ */
+public class test {
+   public static void main(String[] args) {
+        nilai test1 = new nilai();
+        test1.nilai = 12;
+        test1.jenis = 2;
+        
+        test1.run();
+    } 
+}
